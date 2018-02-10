59040c772d637658fc9b2ca4cdfa283f65930eaa
wooojek <yokomura123@gmail.com>
2018-02-10 09:54:18 +0100

    changed imports to support heroku deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const URL = require('../../.env').URL;
+// const URL = require('../../.env').URL;
+const URL = 'mongodb://localhost:27017/TodoApp';
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || URL);
