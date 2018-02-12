bb3692498120d9a9343e675e079e78f92da1c27b
wooojek <yokomura123@gmail.com>
2018-02-12 21:27:36 +0100

    Added tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -119,8 +119,6 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
-// POST /users/login
-
 app.post('/users/login', (req, res) => {
     const body = _.pick(req.body, ['email', 'password']);
 
